0cc346fb855882a3d3ec424ec125e19ed02c6414
Ingi-Hong <ingihong@bu.edu>
2022-11-28 22:33:46 -0500

    added try catch blocks

--- app.py
+++ app.py
@@ -8,18 +8,23 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 # Load in .env variables to connect to PostgreSQL server 
-host = os.environ.get('host')
-password = os.environ.get('password')
-username = os.environ.get('username')
-port = os.environ.get('port')
-database = os.environ.get('database')
-sslmode = os.environ.get('sslmode')
+try:
+    host = os.environ.get('host')
+    password = os.environ.get('password')
+    username = os.environ.get('username')
+    port = os.environ.get('port')
+    database = os.environ.get('database')
+except: 
+    print("failure getting keys")
 
 #Connect to C2
-print("Connecting")
-conn = psycopg2.connect(f"dbname={database} user={username} password={password} host={host} port={port}")
-print("Success? Should be 0: ", conn.closed)
-cursor = conn.cursor()
+try:
+    print("Connecting")
+    conn = psycopg2.connect(f"dbname={database} user={username} password={password} host={host} port={port}")
+    print("Success? Should be 0: ", conn.closed)
+    cursor = conn.cursor()
+except:
+    print("failed to connect")
 
 # For when you want to any query, used to avoid connections timing out 
 def executeQuery(query):
