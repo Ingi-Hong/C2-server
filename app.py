6f4d8a88da1bdd6960e0afed6b4ef1b3a34465f5
Ingi-Hong <ingihong@bu.edu>
2022-11-28 20:49:35 -0500

    Inital Flask App

---
+++ app.py
@@ -0,0 +1,43 @@
+# The C2 Server 
+
+import os
+
+import psycopg2
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+# Load in .env variables to connect to PostgreSQL server 
+host = os.environ['host']
+password = os.environ['password']
+username = os.environ['username']
+port = os.environ['port']
+database = os.environ['database']
+sslmode = os.environ['sslmode']
+
+#Connect to C2
+print("Connecting")
+conn = psycopg2.connect(f"dbname={database} user={username} password={password} host={host} port={port}")
+print("Success? Should be 0: ", conn.closed)
+cursor = conn.cursor()
+
+# For when you want to any query, used to avoid connections timing out 
+def executeQuery(query):
+    cursor.execute(query)
+    returnThis = cursor.fetchall()
+
+    return returnThis 
+
+@app.route("/whoami", methods=["GET"])
+def handle_whoami():
+    #TO IMPLEMENT 
+    pass 
+
+@app.route("/test", methods=["GET"])
+def handle_test():
+    stuff = executeQuery("SELECT * from test")
+    return ("<p>%s</p>" % stuff)
+
+@app.route("/")
+def hello():
+    return "<p>Hello, World!</p>"
